b2e9dd525a240457ed965753a55e8807185f92db
4013563 <4013563@BGHMBCP0414.ad.aricent.com>
2020-09-26 12:51:52 +0530

    addng

--- src/test/java/java_programs/StringPrograms.java
+++ src/test/java/java_programs/StringPrograms.java
@@ -6,11 +6,20 @@ public class StringPrograms {
 
 	public static void main(String[] args) {
 		reverse();
+		duplicateChars();
 	}
 	public static void reverse() {
 		String str="raju";
 		System.out.println(new StringBuffer(str).reverse());
 		
+	}
+	public static  void  duplicateChars() {
+		String str="raju";
+		System.out.println(new StringBuffer(str).reverse());
+		String str1="raju";
+		System.out.println(new StringBuffer(str).reverse());
+		String str2="raju";
+		System.out.println(new StringBuffer(str).reverse());
 	}
 
 }
